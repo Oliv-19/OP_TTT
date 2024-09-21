4c807216b1b11fbe5ef0d4b3c0c4e41a3067894f
Oliv-19 <betza.bruguera@gmail.com>
2024-09-21 11:35:01 -0400

    Add game over Function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let game= (function(){
-    
+
     let player ={
         playerOne: {
             playerName: 'Player 1',
@@ -17,28 +17,28 @@ let game= (function(){
             }else{
                 currentPlayer= this.playerOne
             }
-            
+            domManager.h3Text=currentPlayer.playerName+ "'s turn" 
             return currentPlayer
         },
         changePlayerName: function(e){
             e.preventDefault()
-            
+
             let formData = new FormData(domManager.form)
             this.playerOne.playerName= formData.get('player1')
             this.playerTwo.playerName= formData.get('player2')
-            
+
             domManager.render()
-            
+            domManager.areaListener.abort()
         },
 
     }
-    
+
 
     let currentPlayer = player.playerOne
 
-    console.log(currentPlayer.playerName+ ' turn')
-    console.log('enter cell number:')
-    
+    // console.log(currentPlayer.playerName+ ' turn')
+    // console.log('enter cell number:')
+
     let gameboard={
         board: {
            x:[],
@@ -46,31 +46,31 @@ let game= (function(){
         },
         restart: function(){
             currentPlayer = player.playerOne
-            this.board.x=this.board.x.splice(0, this.board.x) 
-            this.board.o=this.board.o.splice(0, this.board.o) 
+            this.board.x=this.board.x.splice(0, this.board.x)
+            this.board.o=this.board.o.splice(0, this.board.o)
 
         },
+        addPlay: function(e){
+            if(e.target.className == 'cell'){
+                let move = Number(e.target.id )
 
-        addPlay: function(move){
-            if(!this.board.x.includes(move) &&!this.board.o.includes(move)){
-                this.board[currentPlayer.playerMarker].push(move)
-                domManager.render()
                 
-                if(!this.checkWinner()){
-                    player.changePlayer()
+                if(!this.board.x.includes(move) &&!this.board.o.includes(move)){
+                    this.board[currentPlayer.playerMarker].push(move)
                     domManager.render()
-                }
-                
-            } else{
-                console.log('already used')
-            }
-            
-            //console.log(move)
-            console.table(this.board)
-            console.log(currentPlayer.playerName+ ' turn')
-            console.log('enter cell number:')
-            
 
+                    if(!this.checkWinner()){
+                        player.changePlayer()
+                        domManager.render()
+                    }else{
+                        this.gameOver()
+                        domManager.render()
+                    }
+                        
+                } else{
+                    console.log('already used')
+                }
+            }
             
         },
         checkWinner: function(){
@@ -90,11 +90,11 @@ let game= (function(){
             ]
 
             let wins = false
-            
-            
+
+
             winsOP.forEach((array)=>{
                 let arr=[]
-            
+
                 curPlayerArr.forEach(element => {
                     if(array.includes(element)){
                         arr.push(element)
@@ -103,49 +103,52 @@ let game= (function(){
 
                 if( arr.length == 3){
                     console.log(currentPlayer.playerName+' WINS')
-                    console.log('GAME OVER!')
+                    domManager.winnerText= currentPlayer.playerName+' WINS'
                     wins = true
-                    this.restart()
-                    //return true
-                } 
+                }
             })
 
             if(curPlayerArr.length == player.playerOne.maxMoves){
                 console.log('its a draw')
+                domManager.winnerText= "I'ts a draw"
                 wins = true
-                console.log('GAME OVER!')
-                this.restart()
-                //return
             }
-            //console.log(draw)
-            
             return wins
         },
-        
-        
-        
+        gameOver: function(){
+            domManager.h3Text='GAME OVER!'
+            
+            console.log('GAME OVER!')
+            domManager.areaListener.abort()
+
+
+        }
+
+
+
     }
 
     let domManager={
         head: document.querySelector('.head'),
+        h3Text: '',
+        winnerText: '',
         form: document.querySelector('#form'),
         cellContainer: document.querySelector('.cellContainer'),
-        
+        areaListener : new AbortController(),
         listeners:function(){
-            this.form.addEventListener('submit',(e)=>{player.changePlayerName(e)})
-            this.cellContainer.addEventListener('click', (e)=>{this.makePlay(e)})
+            this.form.addEventListener('submit',(e)=>{player.changePlayerName(e)}, {signal: this.areaListener.signal})
+            this.cellContainer.addEventListener('click', (e)=>{gameboard.addPlay(e)}, {signal: this.areaListener.signal})
         },
-        makePlay:function(e){
-            if(e.target.className == 'cell'){
-               gameboard.addPlay(e.target.id)
-            }
-        },
+
         render: function(){
             let h3=this.head.children[0]
-            h3.textContent =currentPlayer.playerName+ "'s turn" 
+            h3.textContent = this.h3Text
 
-            let arr = gameboard.board[currentPlayer.playerMarker]
+            let h5=this.head.children[1]
+            h5.textContent = this.winnerText
             
+            let arr = gameboard.board[currentPlayer.playerMarker]
+
             arr.forEach(cellNum=>{
                 let cell = this.cellContainer.children[cellNum-1]
                 if(currentPlayer.playerMarker== 'x'){
@@ -153,17 +156,12 @@ let game= (function(){
                 }else{
                     cell.style.backgroundColor ='blue'
                 }
-                
+
             })
 
-        
-            
         },
-        
-        
-    } 
-    
+
+    }
 
    return domManager.listeners()
-    // return {makeMove: function(move){gameboard.addPlay(move)},}
 })()
